623e0f217b654f67189198aac29dd757a645a7f3
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-03-07 20:51:55 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
+import Header from './components/Header/Header';
 const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
-
   return (
-    <MyComponent />
+    <Header />
   );
 }
 
