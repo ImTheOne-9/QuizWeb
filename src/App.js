14fb9092920e2d7607a15cf1fae183e50cf5db17
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-03-07 22:34:12 +0700

    Outlet

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import Header from './components/Header/Header';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 const App = () => {
   return (
-    <div className="web-container">
-      <Header />
-      <div>
-        <Link to="/users">User</Link>
-        <Link to="/admins">Amin</Link>
+    <div className="app-container">
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='content-container'>
+          <Outlet />
+        </div>
       </div>
     </div>
 
